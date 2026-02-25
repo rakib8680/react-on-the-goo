ed15681d1002831854f8c65bb4633f04bf369c23
rakib8680 <rakib.cst.1st@gmail.com>
2026-02-25 17:10:43 +0600

    Enhance Countries and Country components with improved styling and layout. Updated Countries component to use a grid layout and added styling to Country component for better presentation of country details.

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,14 +5,16 @@ const Countries = ({ countriesPromise }) => {
 
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
-    
+
     return (
-        <div>
-            <h1>This is Countries component</h1>
-            <h1>Total Countries : {countries.length}</h1>
-            {
-                countries.map(country => <Country key={country.id} country={country} />)
-            }
+        <div className="mt-10">
+            <h1 className="text-2xl font-bold">This is Countries component</h1>
+            <h1 className="text-2xl font-bold">Total Countries : {countries.length}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+                {
+                    countries.map(country => <Country key={country.id} country={country} />)
+                }
+            </div>
         </div>
     )
 };
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,11 +1,22 @@
 
-const Country = ({country}) => {
-        console.log(country);
-  return (
-     <div>
-         <h1>Name: {country.name.common}</h1>
-     </div>
-  )
+const Country = ({ country }) => {
+    console.log(country);
+    return (
+        <div
+            className="border-2 border-gray-400 p-4 rounded-lg hover:bg-gray-100 transition-all duration-300   cursor-pointer hover:shadow-lg ">
+            <h1 className="text-2xl font-bold"><span className="font-medium ">Name:</span> {country.name.common}</h1>
+            <img src={country.flags?.flags?.png} alt={country.flags?.flags?.alt} className="w-full h-48 object-cover rounded-lg mt-4"
+            />
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Capital:</span> {country.capital?.[0]}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Region:</span> {country.region.region}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Population:</span> {country.population}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Area:</span> {country.area}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Timezones:</span> {country.timezones?.join(', ')}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Languages:</span> {country.languages?.languages?.eng}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Currencies:</span> {country.currencies?.currencies}</p> */}
+            {/* <p className="text-sm text-gray-500 mt-2"><span className="font-medium ">Borders:</span> {country.borders?.join(', ')}</p> */}
+        </div>
+    )
 };
 
 export default Country;

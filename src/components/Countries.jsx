2ca1af7d50d8b4bb0e117664d220c65239be63f4
rakib8680 <rakib.cst.1st@gmail.com>
2026-02-26 13:40:02 +0600

    Refactor Country component to improve layout and display additional country details. Updated key prop in Countries component for better uniqueness. Removed unnecessary console log.

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -12,7 +12,7 @@ const Countries = ({ countriesPromise }) => {
             <h1 className="text-2xl font-bold">Total Countries : {countries.length}</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
                 {
-                    countries.map(country => <Country key={country.id} country={country} />)
+                    countries.map(country => <Country key={country.cca3?.cca3} country={country} />)
                 }
             </div>
         </div>
